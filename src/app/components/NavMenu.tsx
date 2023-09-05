cc565847253887dba590cf29347bcfec1532579b
Maykel <jotasenator@gmail.com>
2023-09-05 15:47:24 -0400

    Links with logic

--- src/app/components/NavMenu.tsx
+++ src/app/components/NavMenu.tsx
@@ -3,8 +3,12 @@
 import Link from "next/link";
 
 import { signIn, signOut, useSession } from "next-auth/react";
+import { usePathname } from "next/navigation";
 
-import React from "react";
+const ACTIVE_ROUTE = "py-1 px-2 text-gray-300 bg-gray-700";
+
+const INACTIVE_ROUTE =
+  "py-1 px-2 text-gray-500 hover:text-gray-300 hover:bg-gray-700";
 
 const AuthButton = () => {
   const { data: session } = useSession();
@@ -45,8 +49,27 @@ const AuthButton = () => {
 };
 
 export default function NavMenu() {
+  const pathname = usePathname();
   return (
     <>
+      <hr className="my-4" />
+      <ul>
+        <Link href="/">
+          <li className={pathname == "/" ? ACTIVE_ROUTE : INACTIVE_ROUTE}>
+            Home
+          </li>
+        </Link>
+        <Link href="/protected">
+          <li
+            className={
+              pathname === "/protected" ? ACTIVE_ROUTE : INACTIVE_ROUTE
+            }
+          >
+            Protected Route
+          </li>
+        </Link>
+        <Link href="/serverAction"></Link>
+      </ul>
       <AuthButton />
     </>
   );
